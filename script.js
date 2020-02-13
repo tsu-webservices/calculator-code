25255d2c9c86b6709775e4cf2353cf6e718e538c
Mr.RoBoT404 <joseclaris65@yahoo.com>
2020-02-13 08:53:13 -0600

    Add classification and dropdown for hours

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-function displayRadioValue() { 
-    document.getElementById("result").innerHTML = ""; 
-    var ele = document.getElementsByTagName('input'); 
-      
-    for(i = 0; i < ele.length; i++) { 
-          
-        if(ele[i].type="radio") { 
-          
-            if(ele[i].checked) 
-                document.getElementById("result").innerHTML 
-                        += ele[i].name + " Value: " 
-                        + ele[i].value + "<br>"; 
-        } 
-    } 
-}
+//function displayRadioValue() { 
+//    document.getElementById("result1").innerHTML = ""; 
+//    var ele = document.getElementsByTagName('input'); 
+//      
+//    for(i = 0; i < ele.length; i++) { 
+//          
+//        if(ele[i].type="radio") { 
+//          
+//            if(ele[i].checked) 
+//                document.getElementById("result1").innerHTML 
+//                        += val1 + val2; 
+//        } 
+//    } 
+//}
+
+
+//Reference a form object
+var calcForm = document.forms["calculatorForm"];
