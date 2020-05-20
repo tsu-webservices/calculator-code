d26e8b030f3f9b26180ad905f4d29aa6fd12c104
Silvia Fuentes <silvia.fuen@icloud.com>
2020-05-20 09:46:37 -0500

    Addition of Graduate option
    - Updated estimator to include graduate options
    - added graduate functionality:
    -- hiding rate option
    -- toggle between undergrad(21) and grad(15)
    - reorganized script in grad.js
    - adandoned new.js
    - preview.html - visual code test

--- grad.js
+++ grad.js
@@ -1,22 +1,23 @@
 var semester;
+var student;
 var plan_prices = new Array();
 var services_prices = new Array();
 var servicePrice = 0;
+var gradhours = document.getElementById("gradhours");
+var undergradhours = document.getElementById("undergradhours");
+gradhours.visibility = "hidden";
 
-
-function getStudent() // this reads the value of the selected semester
+function getStudent()//this reads the value of the selected semester
 {
-  student;
+  student = 2;
   var theForm = document.forms["tuitionform"];
   var selectedStudent = theForm.elements["Student"];
   for(var i = 0; i < selectedStudent.length; i++)
   {
-    if(selectedStudent[1].checked)
+    if(selectedStudent[i].checked)
     {
-      student = selectedStudent[1].value;
-        var tuitionrates = document.getElementById('tuitionRates');
-        tuitionrates.style.display = 'none';
-          declareArrays(); // based on the value of the semester selected, this function uses a switch statement to push those values into the arrays
+      student = selectedStudent[i].value;
+          //declareValues(); // based on the value of the semester selected, this function uses a switch statement to push those values into the arrays
           break;
     }
   }
@@ -41,12 +42,12 @@ function getSemester() // this reads the value of the selected semester
   return semester;
 }
 
-function declareUndergradValues() {
+function declareValues() {
   switch (semester) {
     case "0": //full-now (Fall/Spring 2019-2020)
       plan_prices.push(188.58); //0 - Guaranteed - Resident
       plan_prices.push(173.65); //1 - Variable - Resident
-      plan_prices.push(478.57); //2- Grad - Resident
+      plan_prices.push(229.81); //2- Grad - Resident
       services_prices.push(122.53); //0 - Guaranteed - Resident
       services_prices.push(122.53); //1 - Variable - Resident
       services_prices.push(148.76); //2 - Grad - Resident
@@ -55,7 +56,7 @@ function declareUndergradValues() {
     case "1": //half-now (Summer 2019-2020)
       plan_prices.push(94.29); //Guaranteed - Resident
       plan_prices.push(198.09); // Variable - Resident
-      plan_prices.push(239.28); // Grad - Resident
+      plan_prices.push(114.91); // Grad - Resident
       services_prices.push(61.26); //0 - Guaranteed
       services_prices.push(61.26); //1 - Variable - Resident
       services_prices.push(74.38); //2 - Grad - Resident
@@ -100,34 +101,53 @@ function getPlanPrice(){
 }
 
 
-function showHours(){ //slider function
-var slider = document.getElementById("selectedHours");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
-}
-
-function displayHours(){
+function calculateUndergrad(){
   var cost;
   var servicecost;
-  var hours = document.getElementById("semesterHours").value;
+  var hours = document.getElementById("semesterUndergradHours").value;
   var plan = getPlanPrice();
   servicecost = hours * servicePrice;
   cost = hours * plan;
   cost = cost + servicecost + 100.00; // $100 is the Flat fee for Recreation Center
-  cost = cost.toFixed(0); // removes the decimals
+  cost = cost.toFixed(2); // removes the decimals
+   return cost;
+}
+
+function calculateGrad(){
+  var cost;
+  var servicecost;
+  var hours = document.getElementById("semesterGradHours").value;
+  var plan = plan_prices[2]; //identifies which tuition fee is selected
+  servicePrice = services_prices[2]; // identifies which services fee is selected
+  servicecost = hours * servicePrice;
+  cost = hours * plan;
+  cost = cost + servicecost + 100.00; // $100 is the Flat fee for Recreation Center
+  cost = cost.toFixed(2); // removes the decimals
    return cost;
 }
 
 
 function calculateTotal() {
-  getSemester(); // This functions runs prior so the appropriate semester values are loaded into the estimator
-  var tuitionCost = displayHours(); // runs calculations of cost based on number of hours selected
+  var tuitionCost;
+  var tuitionrates = document.getElementById('tuitionRates');
+  if (getStudent() == 1 ){
+   tuitionrates.style.display = "none";
+   undergradhours.style.display = "none";
+   gradhours.style.display = "block";
+   getSemester();
+   tuitionCost = calculateGrad();
+  } else {
+    tuitionrates.style.display = "block";
+    getSemester(); // This functions runs prior so the appropriate semester values are loaded into the estimator
+    tuitionCost = calculateUndergrad(); // runs calculations of cost based on number of hours selected
+  }
+  return tuitionCost;
+}
+
+function displayCost(){
+  var tuitionCost = calculateTotal();
   var divobj = document.getElementById('totalPrice');
-  divobj.style.display = 'block';
+  divobj.style.display = "block";
   divobj.innerHTML = "Estimated Cost of Tuition: $" + tuitionCost; //display the result
 }
 
